62403a470ec4fd7c0b8934e47ac38cf542a30d4c
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:01:54 -0500

    TUnit conversion

--- test/Soenneker.Extensions.Dictionaries.StringString.Tests/DictionaryStringStringExtensionTests.cs
+++ test/Soenneker.Extensions.Dictionaries.StringString.Tests/DictionaryStringStringExtensionTests.cs
@@ -1,11 +1,10 @@
-﻿using Soenneker.Tests.Unit;
-using Xunit;
+using Soenneker.Tests.Unit;
 
 namespace Soenneker.Extensions.Dictionaries.StringString.Tests;
 
 public sealed class DictionaryStringStringExtensionTests : UnitTest
 {
-    [Fact]
+    [Test]
     public void Default()
     {
 
